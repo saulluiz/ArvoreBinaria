843fbc694cb7a109caa49db39af711b6525e34d4
saulluiz <sauloluisoliv@gmail.com>
2024-08-06 11:07:40 -0300

    organizado

--- obj/Debug/net8.0/ArvoresBinaria.AssemblyInfo.cs
+++ obj/Debug/net8.0/ArvoresBinaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArvoresBinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1d395f620a966b294468cefe2f8141e755c6a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9bde52d66b9032abd19569e8ecbe891debb20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArvoresBinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArvoresBinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
